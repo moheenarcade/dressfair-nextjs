50741c2df7727832bbb24fcc3ea78ba7ed1922af
moinkj <moheendealsarcade@gmail.com>
2025-11-07 15:36:52 +0500

    Update mobileHeaderSearchModel.jsx

--- src/components/models/mobileHeaderSearchModel.jsx
+++ src/components/models/mobileHeaderSearchModel.jsx
@@ -50,7 +50,7 @@ const MobileHeaderSearchModel = ({ isOpen, onClose }) => {
         <AnimatePresence>
             {isOpen && (
                 <motion.div
-                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
+                    className="fixed inset-0 z-[999999999999999] flex items-center justify-center bg-black/70"
                     variants={backdrop}
                     initial="hidden"
                     animate="visible"
